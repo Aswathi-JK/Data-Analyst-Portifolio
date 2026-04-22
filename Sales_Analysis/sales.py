57bf105b2d3d566b0ea834866b1d1ff86951484b
Aswathi-JK <aswathijk24@gmail.com>
2026-04-22 18:20:06 +0400

    Updated

--- Sales_Analysis/sales.py
+++ Sales_Analysis/sales.py
@@ -1,9 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv("C:/Dev/Python_projects/sales_data.csv", encoding = "latin1")
+data = pd.read_csv("C:/Dev/Python_projects/sales_data.csv", encoding = "latin1")
 
-print(df.head())
-print(df.columns)
-print(df.info())
+print(data.head())
+print(data.columns)
+print(data.info())
 
-print("Total Sales =", df["Sales"].sum)
+print("Total Sales =", data["Sales"].sum())
+print("Total Profit =", data["Profit"].sum())
+
+print(data.groupby("Region")["Sales"].sum())
+print(data.groupby("Product Category")["Sales"].sum())
+
+loss = data[data["Profit"]<0]
+print(loss[["Product Name", "Profit"]].head())
+print(loss[["Product Category", "Profit"]].head())
+print(data)
